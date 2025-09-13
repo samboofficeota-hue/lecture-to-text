7031aed1e08eca8dc9bed5fedcb4bc31a9e715ec
samboofficeota-hue <samboofficeota-hue@gmail.com>
2025-09-13 20:16:48 +0900

    Test Cloud Run deployment with new service account key

--- cloud_run_api.py
+++ cloud_run_api.py
@@ -172,3 +172,4 @@ def process_audio():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
+# Test with new service account key
